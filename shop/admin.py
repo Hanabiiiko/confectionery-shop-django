ec4467edef5202ae1a56d8c3f6dc605c448ee4e7
Bulakaev Valerii <79519525+Hanabiiiko@users.noreply.github.com>
2026-06-06 19:56:07 +0300

    shop/models Категории и товары

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'weight_grams', 'available', 'created_at')
+    list_filter = ('category', 'available')
+    list_editable = ('available',)
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+    ordering = ('-created_at',)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=200)
+    slug = models.SlugField(verbose_name='Slug', unique=True, max_length=200)
+    image = models.ImageField(verbose_name='Изображение', upload_to='categories/', blank=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/shop/{self.slug}/'
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category,
+        verbose_name='Категория',
+        on_delete=models.CASCADE,
+        related_name='products',
+    )
+    name = models.CharField(verbose_name='Название', max_length=200)
+    slug = models.SlugField(verbose_name='Slug', unique=True, max_length=200)
+    description = models.TextField(verbose_name='Описание', blank=True)
+    price = models.DecimalField(verbose_name='Цена', max_digits=10, decimal_places=2)
+    weight_grams = models.PositiveIntegerField(verbose_name='Вес (г)', null=True, blank=True)
+    image = models.ImageField(verbose_name='Изображение', upload_to='products/')
+    available = models.BooleanField(verbose_name='В наличии', default=True)
+    created_at = models.DateTimeField(verbose_name='Добавлен', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/shop/product/{self.slug}/'
