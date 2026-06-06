de1ca765830f6eb1d38911817e40dfacf8f90778
Bulakaev Valerii <79519525+Hanabiiiko@users.noreply.github.com>
2026-06-06 20:24:54 +0300

    базовый template и css

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from shop.views import HomeView
+
 urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
 ]
 
